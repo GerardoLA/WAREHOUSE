6f3bde65fff1004dd6e0b4dcf1a88d8f9cdb25e9
Usuario <Usuario@DESKTOP-AB3UK2C>
2022-12-28 11:25:59 +0100

    articulo atributos seters y geters

---
+++ src/Proyecto/Articulo.java
@@ -0,0 +1,60 @@
+package Proyecto;
+
+public class Articulo {
+	private String code;
+	private String name;
+	private String mark;
+	private int capadidadBotella;
+	private double precio;
+	private int stock;
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getMark() {
+		return mark;
+	}
+	
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+	
+	public int getCapadidadBotella() {
+		return capadidadBotella;
+	}
+	
+	public void setCapadidadBotella(int capadidadBotella) {
+		this.capadidadBotella = capadidadBotella;
+	}
+	
+	public double getPrecio() {
+		return precio;
+	}
+	
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	public int getStock() {
+		return stock;
+	}
+	
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+
+}
