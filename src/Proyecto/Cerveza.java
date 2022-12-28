1e1780c353edc3905ce4cd7054df56418c24ac84
Usuario <Usuario@DESKTOP-AB3UK2C>
2022-12-28 12:02:33 +0100

    Cerveza atributos seter y geters ok

---
+++ src/Proyecto/Cerveza.java
@@ -0,0 +1,31 @@
+package Proyecto;
+
+public class Cerveza {
+
+	private String origen;
+	private String cereales;
+	private double gradosAlcohol;
+	public String getOrigen() {
+		return origen;
+	}
+	
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+	
+	public String getCereales() {
+		return cereales;
+	}
+	
+	public void setCereales(String cereales) {
+		this.cereales = cereales;
+	}
+	
+	public double getGradosAlcohol() {
+		return gradosAlcohol;
+	}
+	
+	public void setGradosAlcohol(double gradosAlcohol) {
+		this.gradosAlcohol = gradosAlcohol;
+	}
+}
