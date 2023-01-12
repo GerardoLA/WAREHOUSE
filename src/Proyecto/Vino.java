41416020239b8c5a491ea62f218015bb060ec1b7
Usuario <Usuario@DESKTOP-AB3UK2C>
2023-01-13 00:52:50 +0100

    Corregido convertir a minusculas

--- src/Proyecto/Vino.java
+++ src/Proyecto/Vino.java
@@ -46,7 +46,7 @@ public class Vino extends Articulo implements Alcoholico {
 	@Override
 	public boolean saludable() {
 		boolean saludable = false;
-		if(origen.equals("Navarra")) {
+		if(origen.toLowerCase().equals("Navarra")) {
 			
 			saludable = true;
 		}
